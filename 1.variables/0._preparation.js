42d59ad2ee6a03fb9ed7ad3720aa00f7bebc404a
Dewa Gede <50865141+Tri334@users.noreply.github.com>
2023-11-24 11:13:05 +0800

    Preparation for js in vs code

---
+++ 1.variables/0._preparation.js
@@ -0,0 +1,11 @@
+// Download node js terbaru di https://nodejs.org/en
+
+
+// install node js
+// buat file baru
+
+
+// test javascript di terminal vs code dengan klik kanan di folder
+// pilih 'Open in intregeted terminal'
+// ketik
+// node 1._variable.js
---
+++ 1.variables/1._variable.js
@@ -0,0 +1,15 @@
+
+// https://www.w3schools.com/js/js_variables.asp
+
+// Mari deklarasi variable
+// let merupakan variable yang dapat dirubah ubuh secara dynamic dan tidak harus di inisialisasi
+let ini_variable;
+let ini_juga_variable;
+
+
+// const merupakan variable yang akan bersifat tetap (static) dan harus di inisialisasi
+const suatu_variable = 'sesuatu';
+
+
+console.log(ini_variable);
+console.log(suatu_variable);
