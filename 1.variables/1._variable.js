af486d4ba36d49e0be68338d52e17d0bb24533a3
Dewa Gede <50865141+Tri334@users.noreply.github.com>
2023-11-24 19:36:07 +0800

    Add Variable & Object

--- 1.variables/1._variable.js
+++ 1.variables/1._variable.js
@@ -3,7 +3,7 @@
 
 // Mari deklarasi variable
 // let merupakan variable yang dapat dirubah ubuh secara dynamic dan tidak harus di inisialisasi
-let ini_variable;
+let ini_variable=200;
 let ini_juga_variable;
 
 
@@ -11,5 +11,19 @@ let ini_juga_variable;
 const suatu_variable = 'sesuatu';
 
 
+// var merupakan deklarasi variable untuk browser lama, gunakan let & const untuk masa depan
+var sebuah_variable;
+var isi_var = 10;
+
+console.log(isi_var);
 console.log(ini_variable);
 console.log(suatu_variable);
+
+
+// Hoisting adalah alur pemrograman dimulai dari line 1 - seterusnya
+
+// Deklarasi suatau variable
+
+let suatu_var;
+suatu_var = 10;
+console.log(suatu_var);
---
+++ 1.variables/2._object.js
@@ -0,0 +1,22 @@
+// membuat object
+
+
+let animal ={
+    name: 'Jerapah',
+    age : 5,
+    food : ['grass','banana']
+}
+
+console.log(animal.name);
+console.log(animal.age);
+console.log(animal.food);
+
+
+// check type variable
+
+let check_animal_name = typeof(animal.name);
+console.log(check_animal_name); 
+let check_animal_age = typeof(animal.age);
+console.log(check_animal_age); 
+let check_animal_food = typeof(animal.food);
+console.log(check_animal_food); 
